5711bc76be942d6e970a571292da9b09b2499c9a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-18 21:44:14 +0100

    shadcn ui input

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,11 +1,20 @@
 "use client";
-import { Form, 
-FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {UserValidation} from '@/lib/validations/user';
+import { UserValidation } from "@/lib/validations/user";
+import * as z from "zod";
 interface Props {
   user: {
     id: string;
@@ -20,13 +29,66 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const form = useForm({
     resolver: zodResolver(UserValidation),
-    defaultValues:{
-        profile_photo:'',
-        name:'',
-        username:'',
-        bio:'',
-    }
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
   });
-  return (<Form></Form>);
+  const handleImage = (e: any, onChange: any) => {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      onChange(reader.result);
+    };
+  };
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="profile_photo"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="profile_photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src="/assets/profile.svg"
+                    alt="profile_photo"
+                    width={24}
+                    height={24}
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input accept="image/*" type="file" placeholder="Upload a photo" className="account-form_image-input" onChange={(e)=>handleImage(e, field.onchange)} />
+              </FormControl>
+              <FormDescription>Must be unique</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 };
 export default AccountProfile;
