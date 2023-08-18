5774f0f553801bff9445f3c2de68e7b8c94ad645
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-18 20:54:16 +0100

    zod

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,21 +1,22 @@
 "use client";
-import {Form} from '@/components/ui/form';
-import {useForm} from 'react-hook-form'
-interface Props{
-    user: {
-        id: string;
-        objectId: string;
-        username: string;
-        name: string;
-        bio: string;
-        image: string;
-    };
-    btnTitle: string;
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
 }
-const AccountProfile =({user, btnTitle}:Props)=>{
-    const form = useForm()
-    return(
-        <Form></Form>
-    )
-}
+const AccountProfile = ({ user, btnTitle }: Props) => {
+  const form = useForm({
+    resolver: zodResolver(),
+  });
+  return <Form></Form>;
+};
 export default AccountProfile;
