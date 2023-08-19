efb0aaa528f3444feb2a3035bc1ffb8bfa09ef2b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-19 18:47:55 +0100

    error

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,4 +1,11 @@
 "use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { useOrganization } from "@clerk/nextjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+
 import {
   Form,
   FormControl,
@@ -9,55 +16,50 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
-// import { updateUser } from "@/lib/actions/user.actions";
-import { ThreadValidation } from "@/lib/validations/thread";
-import { useRouter, usePathname } from "next/navigation";
-import { createThread } from "@/lib/actions/thread.action";
-interface Props {
-  user: {
-    id: string;
-    objectId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-  };
-  btnTitle: string;
-}
-function PostThread({ userId }: { userId: string }) {
-  const pathname = usePathname();
-  const router = useRouter();
 
-  const form = useForm({
+import { ThreadValidation } from "@/lib/validations/thread";
+import { createThread } from "@/lib/actions/thread.action";
+
+interface Props {
+  userId: string;
+}
+
+function PostThread({ userId }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { organization } = useOrganization();
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
     defaultValues: {
       thread: "",
       accountId: userId,
     },
   });
+
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
     await createThread({
-        text: values.thread,
-        author: userId,
-        communityId:null,
-        path:pathname
+      text: values.thread,
+      author: userId,
+      communityId: organization ? organization.id : null,
+      path: pathname,
     });
-    router.push('/');
+
+    router.push("/");
   };
+
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
         className="mt-10 flex flex-col justify-start gap-10"
+        onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
           name="thread"
           render={({ field }) => (
-            <FormItem className="flex flex-col gap-3 w-full">
+            <FormItem className="flex w-full flex-col gap-3">
               <FormLabel className="text-base-semibold text-light-2">
                 Content
               </FormLabel>
@@ -68,6 +70,7 @@ function PostThread({ userId }: { userId: string }) {
             </FormItem>
           )}
         />
+
         <Button type="submit" className="bg-primary-500">
           Post Thread
         </Button>
@@ -75,4 +78,5 @@ function PostThread({ userId }: { userId: string }) {
     </Form>
   );
 }
+
 export default PostThread;
--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 export const ThreadValidation = z.object({
-  thread: z.string().nonempty().min(3,{message: "Minimum 3 characters"}).max(1000,{message: "Maximum 1000 characters"}),
-  accountId: z.string().nonempty(),
+  thread: z.string().nonempty().min(3,{message: "Minimum 3 characters"}),
+  accountId: z.string(),
 });
 export const CommentValidation = z.object({
     thread: z.string().nonempty().min(3,{message: "Minimum 3 characters"}),
