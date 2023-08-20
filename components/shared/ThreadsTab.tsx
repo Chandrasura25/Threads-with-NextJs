2d537369bd8a7c18b70e6498f80ca5da966b59d3
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 17:27:33 +0100

    thread tab

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,4 +1,11 @@
-const ThreadsTab =()=>{
+interface Props{
+  currentUserId:string;
+              accountId:string;
+              accountType:string;
+}
+const ThreadsTab =({currentUserId,
+  accountId,
+  accountType}:Props)=>{
   return(
     
   )
