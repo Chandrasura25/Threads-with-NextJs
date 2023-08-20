79570a2fbc12b40b7042ec18ab43207ec4e3ace0
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 18:39:44 +0100

    user card

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,6 +1,6 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { profileTabs } from "@/constants";
 import Image from "next/image";
@@ -12,11 +12,28 @@ async function Page() {
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+      <div className="flex flex-col mt-14 gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard />
+            ))}
+          </>
+        )}
+      </div>
     </section>
   );
-};
+}
 
 export default Page;
