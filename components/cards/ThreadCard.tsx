e882c37ea8a9fd3bae51d26c23570c83dadf7ca2
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 05:02:28 +0100

    thread card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -32,7 +32,17 @@ const ThreadCard = ({
   comments,
 }: Props) => {
     return (
-        <article>
+        <article className="flex w-full flex-col rounded-xl bg-dark-2 p-7">
+            <div className="flex items-start justify-between">
+                <div className="flex w-full flex-1 flex-row gap-4">
+                    <div className="flex flex-col items-center">
+                        <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+                            <Image src={author.image} alt="Profile Image" fill className="cursor-pointer rounded-full" />
+                        </Link>
+                        <div className='thread-card_bar' />
+                    </div>
+                </div>
+            </div>
         <h2 className="text-small-regular text-light-2">{content}</h2>
         </article>
     )
