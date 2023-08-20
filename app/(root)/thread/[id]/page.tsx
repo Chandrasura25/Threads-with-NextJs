ef8449bf1fdd6f6ce7bf3195087a472d17f2fc97
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 05:30:41 +0100

    single thread

---
+++ app/(root)/thread/[id]/page.tsx
@@ -0,0 +1,2 @@
+const Page = ({ params }: { params: { id: string } }) => {};
+export default Page;
--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -90,7 +90,13 @@ const ThreadCard = ({
                   className="cursor-pointer object-contain"
                 />
               </div>
-              {isComment && commen}
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
             </div>
           </div>
         </div>
