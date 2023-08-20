1450904c2c9df2ecd17316a6461c5296545846b0
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 19:03:26 +0100

    activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,18 @@
-const Page = async () => {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Image from "next/image";
 import { Button } from "../ui/button";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id: string;
