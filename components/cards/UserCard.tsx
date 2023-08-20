e82dee82bb31bfdd2eee84b1c568e7d7ed0ceac8
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 18:49:01 +0100

    search

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,13 +1,26 @@
+import Image from "next/image";
+
 interface Props{
-    key: string;
     id: string;
     name: string;
     username: string;
     imgUrl: string;
     personType: string;
 }
-const UserCard = ({ key, id, name, username, imgUrl, personType }: Props) => {
-  return <div>UserCard</div>;
+const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+  return (
+    <article className="user-card">
+        <div className="user-card_avatar">
+            <Image
+            src={imgUrl}
+            alt="logo"
+            width={48}
+            height={48}
+            className="rounded-full"
+            />
+        </div>
+    </article>
+  )
 };
 
 export default UserCard;
