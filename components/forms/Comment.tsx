680996f8f1d1a16538998e690281ad0ce7fd70d9
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 14:08:19 +0100

    comments

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -10,25 +10,71 @@ import {
   Form,
   FormControl,
   FormField,
-  FormItem, 
+  FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
 
-import { ThreadValidation } from "@/lib/validations/thread";
+import { CommentValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.action";
-interface Props{
-    threadId:string;
-    currentUserImg:string;
-    currentUserId:string;
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
 }
-const Comment =({threadId, currentUserImg, currentUserId}:Props)=>{
-    return(
-        <div>
-            <h1 className='text-white'>Comment Form</h1>
-        </div>
-    )
-}
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+     const router = useRouter();
+  const pathname = usePathname();
+
+  const { organization } = useOrganization();
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+   await createThread({
+      text: values.thread,
+      author: userId,
+      communityId:  null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
+  return (
+    <Form {...form}>
+      <form
+        className="mt-10 flex flex-col justify-start gap-10"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
+};
 export default Comment;
