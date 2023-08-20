af9be8b74aa937e11477677d1d59a8ec56381255
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 17:49:17 +0100

    thread tab

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -12,18 +12,30 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
   if (!result) redirect("/");
   return (
     <section className="mt-9 flex flex-col gap-10">
-      {result.threads.map((thread) => (
+      {result.threads.map((thread: any) => (
         <ThreadCard
-        key={thread._id}
-        id={thread._id}
-        currentUserId={user?.id || ""}
-        parentId={thread.parentId}
-        content={thread.text}
-        author={thread.author}
-        community={thread.community}
-        createdAt={thread.createdAt}
-        comments={thread.children}
-      />
+          key={thread._id}
+          id={thread._id}
+          currentUserId={currentUserId}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={
+            accountType === "User"
+              ? {
+                  name: result.name,
+                  image: result.image,
+                  id: result.id,
+                }
+              : {
+                  name: thread.author.name,
+                  image: thread.author.image,
+                  id: thread.author.id,
+                }
+          }
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
+        />
       ))}
     </section>
   );
