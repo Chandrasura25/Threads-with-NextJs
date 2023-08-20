e42f5e422f9ec97078cf2f475da1e9a7c6584304
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-20 15:18:01 +0100

    thread card and profile

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -30,7 +30,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
       <div className="mt-7">
         <Comment 
         threadId={thread._id}
-        currentUserImg={user.imageUrl}
+        currentUserImg={userInfo.image}
         currentUserId={JSON.stringify(userInfo._id)}
         />
       </div>
