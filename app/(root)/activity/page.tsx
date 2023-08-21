b34ac539f55fa747ac4bb5ed04279131881e853b
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-21 18:47:02 +0100

    activity done

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -15,21 +15,34 @@ async function Page() {
     <section>
       <h1 className="head-text mb-10">Activity</h1>
       <section className="mt-10 flex flex-col gap-5">
-        {activity.length> 0 ? (
+        {activity.length > 0 ? (
           <>
-          {activity.map((activity)=>(
-            <Link href={`/thread/${activity.parentId}`} key={activity._id}>
-              <article className="activity-card">
-                <Image src={activity.author.image} alt="Profile Picture" width={20} height={20} className="rounded-full object-contain" />
-              </article>
-            </Link>
-          ))}
+            {activity.map((activity) => (
+              <Link href={`/thread/${activity.parentId}`} key={activity._id}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-contain"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
           </>
-          ):<p className="!text-base-regular text-light-3">No Activity Yet</p>
-          }
+        ) : (
+          <p className="!text-base-regular text-light-3">No Activity Yet</p>
+        )}
       </section>
     </section>
   );
-};
+}
 
 export default Page;
