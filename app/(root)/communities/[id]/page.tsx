cfb7e1085749d08f98f342c689f1fa36f5f252ac
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-22 12:21:20 +0100

    communities

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -1,27 +1,24 @@
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import { fetchUser } from "@/lib/actions/user.actions";
+import { communityTabs } from "@/constants";
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
-import { profileTabs } from "@/constants";
-import Image from "next/image";
 import ThreadsTab from "@/components/shared/ThreadsTab";
+import { fetchCommunityDetails } from "@/lib/actions/community.actions";
 
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
-
-  const userInfo = await fetchUser(params.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
+  const communityDetails = await fetchCommunityDetails(params.id);
   return (
     <section>
       <ProfileHeader
-        accountId={userInfo.id}
+        accountId={communityDetails.id}
         authUserId={user.id}
-        name={userInfo.name}
-        username={userInfo.username}
-        imgUrl={userInfo.image}
-        bio={userInfo.bio}
+        name={communityDetails.name}
+        username={communityDetails.username}
+        imgUrl={communityDetails.image}
+        bio={communityDetails.bio}
       />
       <div className="mt-9">
         <Tabs defaultValue="threads" className="w-full">
